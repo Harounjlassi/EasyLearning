660ace6488f5f4bd2ba804d6b51edc7d4b8d67f0
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-30 12:48:20 +0100

    TopBannerClass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import TopBanner from "./components/TopBanner/TopBanner";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TopBanner />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from "react";
+import Container from "react-bootstrap/Container";
+import "../../asset/css/custom.css";
+
+class TopBanner extends Component {
+  render() {
+    return (
+      <Fragment>
+        {/* <div className="row">
+        </div> */}
+        <Container className="topFixedBanner"></Container>
+      </Fragment>
+    );
+  }
+}
+
+export default TopBanner;
