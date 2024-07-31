2cbff675e404b05747e4289536ed9125e2e39f46
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-31 16:59:05 +0100

    customScrollBAr

--- src/components/TopNavigation/TopNavigation.jsx
+++ src/components/TopNavigation/TopNavigation.jsx
@@ -4,6 +4,22 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 export class TopNavigation extends Component {
+  constructor() {
+    super();
+    this.state = {
+      navBarTitle: "navTitle ",
+    };
+  }
+  onScroll = () => {
+    if (window.scrollY > 100) {
+      this.setState({ navBarTitle: "navTitleScroll" });
+    } else if (window.scrollY < 100) {
+      this.setState({ navBarTitle: "navTitle" });
+    }
+  };
+  componentDidMount() {
+    window.addEventListener("scroll", this.onScroll);
+  }
   render() {
     return (
       <Fragment>
@@ -15,31 +31,19 @@ export class TopNavigation extends Component {
           variant="dark"
         >
           <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand className={this.state.navBarTitle} href="#home">
+              EASY LEARNING
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Something
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
+              <Nav className="me-auto"></Nav>
               <Nav>
-                <Nav.Link href="#deets">More deets</Nav.Link>
-                <Nav.Link eventKey={2} href="#memes">
-                  Dank memes
-                </Nav.Link>
+                <Nav.Link href="#deets">HOME</Nav.Link>
+                <Nav.Link href="#deets">ABOUT </Nav.Link>
+                <Nav.Link href="#deets">SERVICE</Nav.Link>
+                <Nav.Link href="#deets">COURSES</Nav.Link>
+                <Nav.Link href="#deets">PORTFOLIO</Nav.Link>
+                <Nav.Link href="#deets">CONTACT US</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
