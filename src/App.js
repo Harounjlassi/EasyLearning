53b01a3a7ec5be33db5d5fd1e31cce9cfd93e47c
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-31 14:27:14 +0100

    addTopNavigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import TopBanner from "./components/TopBanner/TopBanner";
 import Services from "./components/Services/Services";
+import TopNavigation from "./components/TopNavigation/TopNavigation";
 
 function App() {
   return (
     <div className="App">
+      <TopNavigation />
+
       <TopBanner />
       <Services />
     </div>
---
+++ src/components/TopNavigation/TopNavigation.jsx
@@ -0,0 +1,52 @@
+import React, { Component, Fragment } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+export class TopNavigation extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Navbar
+          collapseOnSelect
+          fixed="top"
+          expand="lg"
+          bg="dark"
+          variant="dark"
+        >
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#deets">More deets</Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                  Dank memes
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Fragment>
+    );
+  }
+}
+
+export default TopNavigation;
